97663998777679ccf7cb0fd7880af42d95a1c388
joromemortera <joromethegreat@gmail.com>
2016-08-04 17:31:09 +0800

    deleted frontend/config/.gitignore

---
+++ application/advanced/backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'wJpQ8eNf5lY0Ikz_6sh2i7SOYiu2N6FP',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
